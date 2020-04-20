084ad9be37222c3aa37ec63156d6587f78af210f
Philipp Bischoff <bischoff.phil@gmail.com>
2020-04-20 22:57:54 +0200

    add methods to tree-file, add traversal.rb

--- lib/node.rb
+++ lib/node.rb
@@ -4,15 +4,16 @@
 # instantiates nodes for binary search trees
 class Node
   include Comparable
-  attr_reader :left, :right, :data
+  attr_accessor :left, :right
+  attr_reader :data
+
+  def initialize(data = nil, left = nil, right = nil)
+    @data = data
+    @left = left
+    @right = right
+  end
 
   def <=>(other)
     data <=> other.data
-  end
-
-  def initialize(left = nil, right = nil, data = nil)
-    @left = left
-    @right = right
-    @data = data
   end
 end
--- lib/tree.rb
+++ lib/tree.rb
@@ -2,12 +2,51 @@
 # frozen_string_literal: true
 
 require_relative 'node.rb'
+require_relative 'traversal.rb'
 
 # represents the full binary search tree-objects
 class Tree
+  include Traversal
   attr_reader :root
 
+  def initialize(array)
+    @root = build_tree(array.uniq)
+  end
+
+  def insert(value)
+    line_up(Node.new(value)) unless find(value)
+    self
+  end
+
+  def delete(value); end
+
+  def find(value)
+    node = root
+    until node.nil?
+      return node if node.data == value
+
+      node = (value < node.data ? node.left : node.right)
+    end
+  end
+
+  private
+
   def build_tree(array)
-    #
+    root_node = Node.new(array.shift)
+    array.each do |i|
+      line_up(Node.new(i), root_node)
+    end
+    root_node
+  end
+
+  def line_up(node, root_node = root)
+    other = root_node
+    loop do
+      move = (node < other ? other.left : other.right)
+      break if move.nil?
+
+      other = move
+    end
+    node < other ? other.left = node : other.right = node
   end
 end
